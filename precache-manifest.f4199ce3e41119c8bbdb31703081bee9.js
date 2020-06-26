3de29436e09d0899925cae89c0cd5c1c4c43cc28
Julie <julienne.bay@gmail.com>
2020-06-26 14:43:59 -0400

    Updates

---
+++ precache-manifest.f4199ce3e41119c8bbdb31703081bee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd14c7edf479dd4c66f325f3648d9922",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "93a7415f5f9f3b0fa1c9",
+    "url": "/tictactoe/static/css/main.3002e403.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/tictactoe/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a7415f5f9f3b0fa1c9",
+    "url": "/tictactoe/static/js/main.bb3ff78f.chunk.js"
+  },
+  {
+    "revision": "4434fef27fe19069e69b",
+    "url": "/tictactoe/static/js/runtime-main.666fab5a.js"
+  }
+]);
